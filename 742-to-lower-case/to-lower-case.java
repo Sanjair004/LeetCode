ab2a87e9c0aaf1fa94ae7edf13a1f054b579ded4
SANJAI R <rsanjai179@gmail.com>
2026-08-20 19:18:01 +0530

    Time: 3 ms (9.51%) | Memory: 43.6 MB (5.16%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String v="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
+              ch=(char)(ch+32);
+            }
+            v=v+ch;
+
+        }
+        return v;
+    }
+}
