a6aece9a3a3169597453686125bb5046fd264b6f
SANJAI R <rsanjai179@gmail.com>
2026-08-21 18:50:40 +0530

    Time: 244 ms (5.33%) | Memory: 48 MB (6.64%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        String newString="";
+        String rev="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if((ch>='A' && ch<='Z')||(ch>='0' && ch<='9')||ch>='a' && ch<='z')
+            {
+                newString=newString+ch;
+            }
+        }
+        for(int j=newString.length()-1;j>=0;j--)
+        {
+            char ch=newString.charAt(j);
+            rev=rev+ch;
+        }
+        if(newString.equalsIgnoreCase(rev))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
