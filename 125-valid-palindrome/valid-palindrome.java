e713fe336ef5eaeb81e014371bb8ccede35fbb39
SANJAI R <rsanjai179@gmail.com>
2026-08-21 18:58:47 +0530

    Time: 239 ms (5.33%) | Memory: 48 MB (6.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,4 +1,4 @@
-
+import java.lang.Character;
 class Solution {
     public boolean isPalindrome(String s) {
         String newString="";
@@ -6,10 +6,15 @@ class Solution {
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if((ch>='A' && ch<='Z')||(ch>='0' && ch<='9')||ch>='a' && ch<='z')
+           // if((ch>='A' && ch<='Z')||(ch>='0' && ch<='9')||ch>='a' && ch<='z')
+           // {
+           //     newString=newString+ch;
+           // }
+           if(Character.isLetter(ch) || Character.isDigit(ch))
             {
                 newString=newString+ch;
             }
+           
         }
         for(int j=newString.length()-1;j>=0;j--)
         {
